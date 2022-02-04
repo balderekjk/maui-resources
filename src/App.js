c0e50b1a1454bb1d6a562d8d5a8b8bb3880fa1cd
balderekjk <bal.derek.j@gmail.com>
2022-02-03 17:59:55 -1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [resourceList, setResourceList] = useState([]);
+
+  const getResources = () => {
+    axios.get('http://localhost:3001/resources').then((response) => {
+      setResourceList(response.data);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="kupuna">
+      <nav className="nav">
+        <ul>
+          <li onClick={getResources}>Housing</li>
+          <li>Homeless Outreach</li>
+          <li>Food & Transportation</li>
+          <li>Medical</li>
+          <li>Rental Assistance</li>
+          <li>Childcare</li>
+          <li>Other</li>
+        </ul>
+      </nav>
+      <div className="container">
+        {resourceList.map((val) => {
+          return (
+            <div className="card">
+              <h3>{val.company}</h3>
+              <p>
+                Phone:{' '}
+                <a href={`tel:${val.primary_phone}`}>{val.primary_phone}</a>
+              </p>
+              <p>Address: {val.address}</p>
+              <p>
+                Email: <a href={`mailto:${val.email}`}>{val.email}</a>
+              </p>
+              <p>
+                Website: <a href={val.website}>{val.website}</a>
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
