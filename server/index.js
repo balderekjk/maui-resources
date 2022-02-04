225331f5d6ed782157a6b8d00f401d7e3dba4fe9
balderekjk <bal.derek.j@gmail.com>
2022-02-03 18:03:30 -1000

    both client and server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: 'root',
+  host: 'localhost',
+  password: 'password',
+  database: 'maui_resource_system',
+});
+
+app.get('/resources', (req, res) => {
+  db.query('SELECT * FROM resources', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log('successfully on port 3001');
+});
